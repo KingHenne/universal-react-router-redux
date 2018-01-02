ffa5dc4d4a14e2382c25a464b4a3a2ab1e7a7f47
Hendrik Liebau <mail@hendrik-liebau.de>
2018-01-02 20:35:52 +0100

    Create project with `hops init`
    hops init hops7-enhance-element --template hops-template-react@7.2.0

---
+++ src/home/spec/home.spec.js
@@ -0,0 +1,12 @@
+// @flow
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Home } from '../';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <Home />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
