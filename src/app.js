f32666f020f55c2c8637e40132ea6e12522cfbd9
Hendrik Liebau <mail@hendrik-liebau.de>
2018-01-04 15:13:33 +0100

    Add redirect route and render location info

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import {Redirect} from 'react-router';
 import {Route, Switch, Link} from 'react-router-dom';
 
 import {Miss} from 'hops-react';
 
 import {Home} from './home';
 import {Counter} from './counter';
+import {LocationInfo} from './locationInfo';
 
 export const App = () => (
   <div>
     <nav>
-      <Link to="/">Home</Link>&nbsp;
+      <Link to="/home">Home</Link>&nbsp;
       <Link to="/counter">Counter</Link>
     </nav>
+    <LocationInfo />
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" render={() => <Redirect to="/home" />} />
+      <Route exact path="/home" component={Home} />
       <Route exact path="/counter" component={Counter} />
       <Miss />
     </Switch>
