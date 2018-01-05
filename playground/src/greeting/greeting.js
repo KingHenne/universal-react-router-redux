f4009fd360527c4709987dbd116cda91b39268b7
Hendrik Liebau <mail@hendrik-liebau.de>
2018-01-05 11:13:11 +0100

    Move relevant playground files into src

--- playground/src/greeting/greeting.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Greeting({className, visitor = 'World'}) {
-  return <h1 className={className}>Hello {visitor}!</h1>;
-}
--- playground/src/greeting/greetingContainer.js
+++
@@ -1,9 +0,0 @@
-import {connect} from 'react-redux';
-
-import Greeting from './greeting';
-
-export const mapStateToProps = ({router}) => ({
-  visitor: router.match.params.visitor,
-});
-
-export default connect(mapStateToProps)(Greeting);
